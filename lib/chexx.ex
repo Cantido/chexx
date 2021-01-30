2b2e47a3683f7390cb4ba9380134cd2439ef7816
Rosa Richter <cosmic.lady.rosa@gmail.com>
2021-01-29 18:04:06 -0700

    basic pawn movement and capture

--- lib/chexx.ex
+++ lib/chexx.ex
@@ -3,16 +3,142 @@ defmodule Chexx do
   Documentation for `Chexx`.
   """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Chexx.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def new do
+    []
   end
+
+  def put_piece(board, type, color, square) do
+    if not is_nil(piece_at(board, square)) do
+      raise "That square already has a piece"
+    end
+
+    if not is_valid_square(square) do
+      raise "Not a valid place to put a piece"
+    end
+
+    [%{type: type, color: color, square: square} | board]
+  end
+
+  defp is_valid_square({file, rank}) do
+    file in [:a, :b, :c, :d, :e, :f, :g, :h] and rank in 1..8
+  end
+
+  def delete_piece(board, square) do
+    Enum.reject(board, fn piece ->
+      piece.square == square
+    end)
+  end
+
+  def piece_at(board, square) do
+    board
+    |> Enum.find(fn piece ->
+      piece.square == square
+    end)
+    |> case do
+      nil -> nil
+      piece ->
+        Map.take(piece, [
+          :type,
+          :color
+        ])
+    end
+  end
+
+  @pawn_notation ~r/^[a-h][1-8]$/
+  @pawn_capture_notation ~r/^[a-h]x[a-h][1-8]$/
+
+  def move(board, by, movement) do
+    destination =
+      cond do
+        String.match?(movement, @pawn_notation) ->
+          file = String.at(movement, 0) |> String.to_existing_atom()
+          {rank, ""} = String.at(movement, 1) |> Integer.parse()
+          {file, rank}
+        String.match?(movement, @pawn_capture_notation) ->
+          file = String.at(movement, 2) |> String.to_existing_atom()
+          {rank, ""} = String.at(movement, 3) |> Integer.parse()
+          {file, rank}
+      end
+
+    possible_starting_positions =
+      cond do
+        String.match?(movement, @pawn_notation) ->
+          {_file, rank} = destination
+          case {by, rank} do
+            {:white, 4} -> [down(destination, 1), down(destination, 2)]
+            {:white, _} -> [down(destination, 1)]
+            {:black, 4} -> [up(destination, 1), up(destination, 2)]
+            {:black, _} -> [up(destination, 1)]
+          end
+        String.match?(movement, @pawn_capture_notation) ->
+          starting_file = String.at(movement, 0) |> String.to_existing_atom()
+          {_file, destination_rank} = destination
+          case by do
+            :white -> [down({starting_file, destination_rank}, 1)]
+            :black -> [up({starting_file, destination_rank}, 1)]
+          end
+      end
+
+    possible_source_spaces =
+      possible_starting_positions
+      |> Enum.filter(fn possible_starting_position ->
+        piece_at(board, possible_starting_position)
+      end)
+
+    if Enum.empty?(possible_source_spaces) do
+      raise "No piece found for #{by} to perform move #{inspect movement}"
+    end
+
+    source_space = Enum.at(possible_source_spaces, 0)
+    moving_piece = piece_at(board, source_space)
+
+    captured_piece = piece_at(board, destination)
+
+    if not is_nil(captured_piece) and captured_piece.color == by do
+      raise "Cannot capture your own piece!"
+    end
+
+    board
+    |> delete_piece(source_space)
+    |> delete_piece(destination)
+    |> put_piece(moving_piece.type, moving_piece.color, destination)
+  end
+
+  defp up({file, rank}, squares) do
+    {file, rank + squares}
+  end
+
+  defp down({file, rank}, squares) do
+    {file, rank - squares}
+  end
+
+  defp left({file, rank}, squares) do
+    new_file = number_to_file(file_to_number(file) - squares)
+    {new_file, rank}
+  end
+
+  defp right({file, rank}, squares) do
+    new_file = number_to_file(file_to_number(file) + squares)
+    {new_file, rank}
+  end
+
+  defp file_to_number(:a), do: 1
+  defp file_to_number(:b), do: 2
+  defp file_to_number(:c), do: 3
+  defp file_to_number(:d), do: 4
+  defp file_to_number(:e), do: 5
+  defp file_to_number(:f), do: 6
+  defp file_to_number(:g), do: 7
+  defp file_to_number(:h), do: 8
+
+  defp number_to_file(1), do: :a
+  defp number_to_file(2), do: :b
+  defp number_to_file(3), do: :c
+  defp number_to_file(4), do: :d
+  defp number_to_file(5), do: :e
+  defp number_to_file(6), do: :f
+  defp number_to_file(7), do: :g
+  defp number_to_file(8), do: :h
+
+
 end
--- test/chexx_test.exs
+++ test/chexx_test.exs
@@ -2,7 +2,130 @@ defmodule ChexxTest do
   use ExUnit.Case
   doctest Chexx
 
-  test "greets the world" do
-    assert Chexx.hello() == :world
+  describe "put_piece" do
+    test "put a piece at a square" do
+      piece =
+        Chexx.new()
+        |> Chexx.put_piece(:rook, :black, {:a, 8})
+        |> Chexx.piece_at({:a, 8})
+
+      assert piece.type == :rook
+      assert piece.color == :black
+    end
+
+    test "piece_at returns nil when square is empty" do
+      piece =
+        Chexx.new()
+        |> Chexx.piece_at({:a, 8})
+
+      assert piece == nil
+    end
+
+    test "can't put a piece on the same square twice" do
+      assert_raise RuntimeError, fn ->
+        Chexx.new()
+        |> Chexx.put_piece(:rook, :black, {:a, 8})
+        |> Chexx.put_piece(:rook, :black, {:a, 8})
+      end
+    end
+
+    test "can't put a piece off the top boundary of the board" do
+      assert_raise RuntimeError, fn ->
+        Chexx.new()
+        |> Chexx.put_piece(:rook, :black, {:a, 9})
+      end
+    end
+
+    test "can't put a piece off the right boundary of the board" do
+      assert_raise RuntimeError, fn ->
+        Chexx.new()
+        |> Chexx.put_piece(:rook, :black, {:i, 8})
+      end
+    end
+
+    test "can't put a piece off the bottom boundary of the board" do
+      assert_raise RuntimeError, fn ->
+        Chexx.new()
+        |> Chexx.put_piece(:rook, :black, {:a, 0})
+      end
+    end
+  end
+
+  describe "pawn moves" do
+    test "can move a white pawn up one square" do
+      piece =
+        Chexx.new()
+        |> Chexx.put_piece(:pawn, :white, {:e, 3})
+        |> Chexx.move(:white, "e4")
+        |> Chexx.piece_at({:e, 4})
+
+        assert piece.type == :pawn
+        assert piece.color == :white
+    end
+
+    test "can move a white pawn up two squares if it is in the starting row" do
+      piece =
+        Chexx.new()
+        |> Chexx.put_piece(:pawn, :white, {:e, 2})
+        |> Chexx.move(:white, "e4")
+        |> Chexx.piece_at({:e, 4})
+
+        assert piece.type == :pawn
+        assert piece.color == :white
+    end
+
+    test "can move a black pawn down one square" do
+      piece =
+        Chexx.new()
+        |> Chexx.put_piece(:pawn, :black, {:e, 6})
+        |> Chexx.move(:black, "e5")
+        |> Chexx.piece_at({:e, 5})
+
+        assert piece.type == :pawn
+        assert piece.color == :black
+    end
+
+    test "can move a piece when two pieces can share a destination, as black" do
+      piece =
+        Chexx.new()
+        |> Chexx.put_piece(:pawn, :black, {:e, 6})
+        |> Chexx.put_piece(:pawn, :white, {:e, 4})
+        |> Chexx.move(:black, "e5")
+        |> Chexx.piece_at({:e, 5})
+
+        assert piece.type == :pawn
+        assert piece.color == :black
+    end
+
+    test "can move a piece when two pieces can share a destination, as white" do
+      piece =
+        Chexx.new()
+        |> Chexx.put_piece(:pawn, :black, {:e, 6})
+        |> Chexx.put_piece(:pawn, :white, {:e, 4})
+        |> Chexx.move(:white, "e5")
+        |> Chexx.piece_at({:e, 5})
+
+        assert piece.type == :pawn
+        assert piece.color == :white
+    end
+
+    test "can't move if there are no pieces that can be moved" do
+      assert_raise RuntimeError, fn ->
+        Chexx.new()
+        |> Chexx.move(:black, "e5")
+      end
+    end
+
+    test "pawn capture" do
+      piece =
+        Chexx.new()
+        |> Chexx.put_piece(:pawn, :black, {:f, 5})
+        |> Chexx.put_piece(:pawn, :white, {:e, 4})
+        |> Chexx.move(:black, "fxe4")
+        |> Chexx.piece_at({:e, 4})
+
+      assert piece.type == :pawn
+      assert piece.color == :black
+    end
   end
 end
