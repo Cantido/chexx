985288affc6618ffe32ce242bb3712093ac726c1
Rosa Richter <cosmic.lady.rosa@gmail.com>
2021-02-08 15:17:23 -0700

    Add unicode symbols for pieces

--- lib/chexx/piece.ex
+++ lib/chexx/piece.ex
@@ -25,4 +25,27 @@ defmodule Chexx.Piece do
   def new(type, color) when is_piece(type) and is_color(color) do
     %__MODULE__{type: type, color: color}
   end
+
+  def to_unicode(%__MODULE__{type: type, color: color}) do
+    case {color, type} do
+      {:white, :king} -> "♔"
+      {:white, :queen} -> "♕"
+      {:white, :rook} -> "♖"
+      {:white, :bishop} -> "♗"
+      {:white, :knight} -> "♘"
+      {:white, :pawn} -> "♙"
+      {:black, :king} -> "♚"
+      {:black, :queen} -> "♛"
+      {:black, :rook} -> "♜"
+      {:black, :bishop} -> "♝"
+      {:black, :knight} -> "♞"
+      {:black, :pawn} -> "♟︎"
+    end
+  end
+
+  defimpl Inspect, for: __MODULE__ do
+    def inspect(piece, _opts) do
+      Chexx.Piece.to_unicode(piece)
+    end
+  end
 end
