a99b9da54a5b9c7492f882c8aa26c029c488ccc1
Rosa Richter <cosmic.lady.rosa@gmail.com>
2021-05-28 12:05:24 -0600

    Document Chexx.Ply
    Why not, it's essential to know if anyone is adding new pieces

--- lib/chexx/ply.ex
+++ lib/chexx/ply.ex
@@ -3,8 +3,25 @@ defmodule Chexx.Ply do
   Encapsulates a change in `Chexx.Piece`'s position on a `Chexx.Board`,
   along with certain requirements of the move.
 
-  A Ply is usually built from a single `Chexx.Touch`, but some moves require multiple touches,
+  A `Ply` is usually built from a single `Chexx.Touch`, but some moves require multiple touches,
   like castling.
+
+  The `Ply` struct has the following keys:
+
+    - `:player` - the player making the move
+    - `:touches` - the actual piece touches that must happen, like moves, promotions, etc.
+    - `:capture` - the move's capture requirements. Can be one of the following values:
+      - `:allowed` - a piece can be at the `:captures` square, but it is not necessary.
+      - `:forbidden` - this move is not allowed to capture any pieces, for example a pawn advance.
+      - `:required` - there *must* be a piece at the `:captures` square, for example in an *en passant* capture.
+    - `:captures` - the square that this ply captures, or nil. Use with the `:capture` value.
+    - `:captured_piece_type` - if set, the captured piece at `:captures` *must* be of the type given.
+       for example, this is set to `:pawn` in an *en passant* capture.
+    - `:traverses` - the squares that are traveled through that must not contain any pieces.
+    - `:vulnerabilities` - a list of the squares that cannot be attacked in order for this move to succeed.
+      For example, a king's destination may never be attacked, neither can the squares traveled by the king during castling.
+    - `:match_history_fn` - a function that evalutates the current game's history, returning `true` if the ply is allowed based on history.
+      You can use this for castling or *en passant* type moves to make sure a piece hasn't moved yet, or has made a certain move.
   """
 
   alias Chexx.Touch
